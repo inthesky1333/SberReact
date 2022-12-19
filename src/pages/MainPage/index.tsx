009b8e0da474432d32b5959f3750cd94d99891d6
anton_galich <inthesky133@gmail.com>
2022-12-19 19:23:29 +0300

    protect pages

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,19 +2,25 @@ import React, { FC, useEffect } from "react";
 
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
+import { LocalPaths } from "@paths/loclalPath";
 import { useAppDispatch } from "@store/index";
 import { me } from "@store/user/userActions";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 import styles from "./mainPage.module.css";
 import { IMainPageProps } from "./mainpageProps";
 
 export const MainPage: FC<IMainPageProps> = () => {
   const dispatch = useAppDispatch();
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     dispatch(me());
   }, []);
+
+  if (!token) {
+    return <Navigate to={`${LocalPaths.Login}`} />;
+  }
 
   return (
     <>
