9e2f3bcd8e93c3b3e599eef82f0f0af6ad31659c
anton_galich <galichanton33@gmail.com>
2023-01-15 10:49:45 +0300

    refactor after review

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
 
 import { Input } from "@components/UI/Input";
 import { useDebounce } from "@hooks/useDebounce";
@@ -9,21 +9,30 @@ import styles from "./searchbar.module.css";
 import { ISearchBarProps } from "./searchbarProps";
 
 export const SearchBar: FC<ISearchBarProps> = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const dispatch = useAppDispatch();
 
   const setSearch = useDebounce((e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(setSearchTerm(e.target.value.toLowerCase()));
   }, 500);
 
+  const clearSearch = () => {
+    dispatch(setSearchTerm(""));
+    inputRef.current!.value = "";
+  };
+
   return (
     <div className={styles.searchbar}>
       <Input
+        ref={inputRef}
         name={"search"}
         placeholder={"Поиск"}
         className={styles.input}
         onChange={setSearch}
       />
-      <span className={styles.close}>X</span>
+      <span className={styles.close} onClick={clearSearch}>
+        X
+      </span>
     </div>
   );
 };
