cd3aadbdbcde5f6d9277052642e10fbaf97bf339
anton_galich <galichanton33@gmail.com>
2023-02-02 15:36:14 +0000

    gh-pages

---
+++ static/js/445.cbf51a98.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_task_1=self.webpackChunkreact_task_1||[]).push([[445],{2445:function(t,e,a){a.r(e);a(2791);var c=a(570),n=a(7689),r=a(184);e.default=function(){var t=localStorage.getItem("token");return(0,r.jsx)(r.Fragment,{children:t?(0,r.jsx)(n.Fg,{to:"".concat(c.V.Products)}):(0,r.jsx)(n.Fg,{to:"".concat(c.V.Login)})})}}}]);
+//# sourceMappingURL=445.cbf51a98.chunk.js.map
