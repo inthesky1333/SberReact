76cf55c9b385ae96a17fd0364ae41cee97c3fc29
anton_galich <inthesky133@gmail.com>
2023-02-02 17:38:03 +0300

    fix filter bug

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,5 +4,10 @@ import styles from "./footer.module.css";
 import { IFooterProps } from "./footerProps";
 
 export const Footer: FC<IFooterProps> = () => {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>Created by Anton Galich</p>
+      <a href={"https://github.com/inthesky1333/SberReact"}>github</a>
+    </footer>
+  );
 };
